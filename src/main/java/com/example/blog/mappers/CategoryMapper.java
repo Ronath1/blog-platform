057b3d1d5918a21336985babe6a385e8112b6a08
Ronath1 <ronathwijayasiri123@gmail.com>
2025-10-03 14:07:56 +0530

    add mapping and repository

---
+++ src/main/java/com/example/blog/mappers/CategoryMapper.java
@@ -0,0 +1,33 @@
+package com.example.blog.mappers;
+
+
+import com.example.blog.domain.PostStatus;
+import com.example.blog.domain.dtos.CategoryDto;
+import com.example.blog.domain.dtos.CreateCategoryRequest;
+import com.example.blog.domain.entities.Category;
+import com.example.blog.domain.entities.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CategoryMapper {
+
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
+    CategoryDto toDto(Category category);
+
+     Category toEntity(CreateCategoryRequest createCategoryRequest);
+
+    @Named("calculatePostCount")
+    default long calculatePostCount(List<Post>posts){
+        if(null == posts){
+            return 0;
+        }
+        return posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
+}
